8f0363fd6d386de85038d5d049e2fc0271b0e1d8
ricardo soto <ricardosr4@gmail.com>
2023-09-10 16:37:32 -0300

    actualizado

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -6,96 +6,89 @@ import android.view.View
 import android.webkit.ClientCertRequest
 import android.widget.Button
 import android.widget.TextView
+import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var oper: Int = 0
-    var numero1: Double = 0.0
-    lateinit var tv_num1: TextView
-    lateinit var tv_num2: TextView
+    private lateinit var binding: ActivityMainBinding
+    private var oper: Int = 0
+    private var numero1: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        tv_num1 = findViewById(R.id.tv_num1)
-        tv_num2 = findViewById(R.id.tv_num2)
-        val btnborrar: Button = findViewById(R.id.btnborrar)
-        val btnigual: Button = findViewById(R.id.btnigual)
+        val btnborrar: Button = binding.btnborrar
+        val btnigual: Button = binding.btnigual
 
         btnigual.setOnClickListener {
-            var numero2: Double = tv_num2.text.toString().toDouble()
+            val numero2: Double = binding.tvNum2.text.toString().toDouble()
             var resp: Double = 0.0
 
-
-            when(oper){
+            when (oper) {
                 1 -> resp = numero1 + numero2
                 2 -> resp = numero1 - numero2
                 3 -> resp = numero1 * numero2
                 4 -> resp = numero1 / numero2
             }
-            tv_num2.setText(resp.toString())
-            tv_num1.setText("")
 
+            // Eliminar ceros innecesarios al final del número
+            val resultadoSinCeros = resp.toString().trimEnd('0').trimEnd('.')
+
+            binding.tvNum2.text = resultadoSinCeros
+            binding.tvNum1.text = ""
         }
 
         btnborrar.setOnClickListener {
-            tv_num1.setText("")
-            tv_num2.setText("")
+            binding.tvNum1.text = ""
+            binding.tvNum2.text = ""
             numero1 = 0.0
             oper = 0
         }
-
     }
 
     fun presionarDigito(view: View) {
-        //val tv_num2: TextView = findViewById(R.id.tv_num2)
-        var num2: String = tv_num2.text.toString()
+        val num2: String = binding.tvNum2.text.toString()
 
         when (view.id) {
-
-            R.id.btn0 -> tv_num2.setText(num2 + "0")
-            R.id.btn1 -> tv_num2.setText(num2 + "1")
-            R.id.btn2 -> tv_num2.setText(num2 + "2")
-            R.id.btn3 -> tv_num2.setText(num2 + "3")
-            R.id.btn4 -> tv_num2.setText(num2 + "4")
-            R.id.btn5 -> tv_num2.setText(num2 + "5")
-            R.id.btn6 -> tv_num2.setText(num2 + "6")
-            R.id.btn7 -> tv_num2.setText(num2 + "7")
-            R.id.btn8 -> tv_num2.setText(num2 + "8")
-            R.id.btn9 -> tv_num2.setText(num2 + "9")
-            R.id.btnpunto -> tv_num2.setText(num2 + ".")
-
-
+            R.id.btn0 -> binding.tvNum2.text = num2 + "0"
+            R.id.btn1 -> binding.tvNum2.text = num2 + "1"
+            R.id.btn2 -> binding.tvNum2.text = num2 + "2"
+            R.id.btn3 -> binding.tvNum2.text = num2 + "3"
+            R.id.btn4 -> binding.tvNum2.text = num2 + "4"
+            R.id.btn5 -> binding.tvNum2.text = num2 + "5"
+            R.id.btn6 -> binding.tvNum2.text = num2 + "6"
+            R.id.btn7 -> binding.tvNum2.text = num2 + "7"
+            R.id.btn8 -> binding.tvNum2.text = num2 + "8"
+            R.id.btn9 -> binding.tvNum2.text = num2 + "9"
+            R.id.btnpunto -> binding.tvNum2.text = num2 + "."
         }
     }
 
     fun clicOperacion(view: View) {
-        numero1 = tv_num2.text.toString().toDouble()
-        var num2_text: String = tv_num2.text.toString()
-        tv_num2.setText("")
-
-
-
+        numero1 = binding.tvNum2.text.toString().toDouble()
+        val num2Text: String = binding.tvNum2.text.toString()
+        binding.tvNum2.text = ""
 
         when (view.id) {
             R.id.btnsumar -> {
-                tv_num1.setText(num2_text + "+")
-                 oper = 1
+                binding.tvNum1.text = num2Text + "+"
+                oper = 1
             }
             R.id.btnrestar -> {
-                tv_num1.setText(num2_text + "-")
-                 oper = 2
+                binding.tvNum1.text = num2Text + "-"
+                oper = 2
             }
             R.id.btnmult -> {
-                tv_num1.setText(num2_text + "*")
-                 oper = 3
+                binding.tvNum1.text = num2Text + "*"
+                oper = 3
             }
             R.id.btndiv -> {
-                tv_num1.setText(num2_text + "/")
-                 oper = 4
+                binding.tvNum1.text = num2Text + "/"
+                oper = 4
             }
         }
-
     }
 }
 

dbe328c2905b9906b87eae66551724611a6dff3c
ricar <ricardosr4@gmail.com>
2024-02-12 20:02:59 -0300

    ok

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,15 +3,13 @@ package com.example.calculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.webkit.ClientCertRequest
 import android.widget.Button
-import android.widget.TextView
 import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var oper: Int = 0
-    private var numero1: Double = 0.0
+    private var operation: Int = 0    // Variable para almacenar el tipo de operación (+, -, *, /)
+    private var numero1: Double = 0.0   // Variable para almacenar el primer número en la operación
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,33 +22,44 @@ class MainActivity : AppCompatActivity() {
 
         btnigual.setOnClickListener {
             val numero2: Double = binding.tvNum2.text.toString().toDouble()
-            var resp: Double = 0.0
+            var response = 0.0
+// Realiza la operación correspondiente según el valor de 'operation'
+            when (operation) {
+                1 -> {
+                    response = numero1 + numero2
+                }
 
-            when (oper) {
-                1 -> resp = numero1 + numero2
-                2 -> resp = numero1 - numero2
-                3 -> resp = numero1 * numero2
-                4 -> resp = numero1 / numero2
+                2 -> {
+                    response = numero1 - numero2
+                }
+
+                3 -> {
+                    response = numero1 * numero2
+                }
+
+                4 -> {
+                    response = numero1 / numero2
+                }
             }
 
-            // Eliminar ceros innecesarios al final del número
-            val resultadoSinCeros = resp.toString().trimEnd('0').trimEnd('.')
+            // Eliminar ceros innecesarios al final del número y muestra el resultado
+            val resultadoSinCeros = response.toString().trimEnd('0').trimEnd('.')
 
             binding.tvNum2.text = resultadoSinCeros
             binding.tvNum1.text = ""
         }
-
+// Limpia los campos y reinicia las variables
         btnborrar.setOnClickListener {
             binding.tvNum1.text = ""
             binding.tvNum2.text = ""
             numero1 = 0.0
-            oper = 0
+            operation = 0
         }
     }
 
     fun presionarDigito(view: View) {
         val num2: String = binding.tvNum2.text.toString()
-
+// Limpia los campos y reinicia las variables
         when (view.id) {
             R.id.btn0 -> binding.tvNum2.text = num2 + "0"
             R.id.btn1 -> binding.tvNum2.text = num2 + "1"
@@ -64,29 +73,33 @@ class MainActivity : AppCompatActivity() {
             R.id.btn9 -> binding.tvNum2.text = num2 + "9"
             R.id.btnpunto -> binding.tvNum2.text = num2 + "."
         }
+        //hola ricardo
     }
 
     fun clicOperacion(view: View) {
         numero1 = binding.tvNum2.text.toString().toDouble()
         val num2Text: String = binding.tvNum2.text.toString()
         binding.tvNum2.text = ""
-
+        // Actualiza la pantalla con la operación seleccionada y el número actual
         when (view.id) {
             R.id.btnsumar -> {
                 binding.tvNum1.text = num2Text + "+"
-                oper = 1
+                operation = 1
             }
+
             R.id.btnrestar -> {
                 binding.tvNum1.text = num2Text + "-"
-                oper = 2
+                operation = 2
             }
+
             R.id.btnmult -> {
                 binding.tvNum1.text = num2Text + "*"
-                oper = 3
+                operation = 3
             }
+
             R.id.btndiv -> {
                 binding.tvNum1.text = num2Text + "/"
-                oper = 4
+                operation = 4
             }
         }
     }
